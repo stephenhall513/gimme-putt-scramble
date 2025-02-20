23472dd71632ecb6ef421512a3af9b1af84db125
stephenhall513 <stephen.j.hall@outlook.com>
2025-02-20 10:45:15 -0500

    Changed to function

--- src/app/events/[eventId]/[scrambleId]/leaderboard/page.tsx
+++ src/app/events/[eventId]/[scrambleId]/leaderboard/page.tsx
@@ -1,10 +1,10 @@
 import Leaderboard from "@/components/Leaderboard/Leaderboard";
 
-const LeaderboardPage = async ({
+export default async function LeaderboardPage({
   params,
 }: {
-  params: { scrambleId: string };
-}) => {
+  params: Promise<{ scrambleId: string }>;
+}) {
   const scrambleId = (await params).scrambleId;
 
   return (
@@ -14,6 +14,4 @@ const LeaderboardPage = async ({
       </div>
     </>
   );
-};
-
-export default LeaderboardPage;
+}
